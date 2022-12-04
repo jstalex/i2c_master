231d369cadcecbf4f946b8b7a13d745f901e46fd
jstalex <90794457+jstalex@users.noreply.github.com>
2022-12-05 01:06:17 +0300

    added part of write tb

--- i2c.srcs/sim_1/new/mstr1_tb.v
+++ i2c.srcs/sim_1/new/mstr1_tb.v
@@ -1,6 +1,6 @@
 `timescale 1ns / 1ps
 
-module mstr_tb();
+module mstr1_tb();
 
 reg scl;
 wire sda;
@@ -12,8 +12,9 @@ reg [6:0] address;
 reg [3:0] state;
 reg [2:0] bit_ind;
 reg [3:0] counter;
-
+reg rw; // read write register
 reg [7:0] data_for_slave;
+reg [7:0] byte;
 reg [7:0] data_for_test;
 
 assign sda = we ? wd : 1'bz;
@@ -31,16 +32,19 @@ parameter LIS = 0,
           GETA = 1,
           ANS = 2,
           SEND = 3,
-          STOP = 4;
+          STOP = 4,
           
+          GETD = 6,
+          GETRW = 5;
 
 
-initial begin	// test reading
+initial begin	// test reading 400нс транзакция с чтением 1 бита
 	mode <= 1'b0;
 	address <= 7'b1101001; // mpu6000
 	data_for_test <= 8'b10101010;
 	bit_ind <= 0;
 	counter <= 0;
+	byte <= 8'b0;
 	state <= LIS;
 	wd <= 1'b0;
 	we <= 1'b0;
@@ -79,23 +83,42 @@ always @(posedge scl) begin
 //	           counter <= 0;
 //	       end
            counter <= counter + 1'b1;
-	       if (counter == 4'd6) begin
+	       if (counter == 4'd7) begin
 	           bit_ind <= 0;
 	           counter <= 0;
-	           state <= ANS;
+	           state <= GETRW;
 	       end
 	   end
+	   GETRW: begin
+	       rw <= sda;
+	       wd <= 0;
+	       we <= 1'b1; // сюда
+	   end
 	   ANS: begin
-	      wd <= 0;
-	      we <= 1'b1;
+	      // сдвинул энейбл на 1 такт вверх
 	      #20;
 	      bit_ind <= 0;
-	      state <= SEND;
+	      we <= 1'b0;
+	      if (mode) state <= GETD;
+	      else state <= SEND;
 	   end
+	   GETD: begin
+	        byte[bit_ind] <= sda;
+            bit_ind <= bit_ind + 1'b1;
+            if (bit_ind == 3'd7) begin
+                bit_ind <= 0;
+                byte <= 8'b0;
+                wd <= 1'b0; // ack answer
+                we <= 1'b1;
+                // отдать шину и все 
+                // тут либо новый стейт сделать, либо однобитные флаги просто с задержкой
+            end
+	   end
+	   
 	   SEND: begin
 	       wd <= data_for_test[bit_ind];
 	       bit_ind <= bit_ind + 1'b1;
-           if (bit_ind == 3'd7)begin
+           if (bit_ind == 3'd7) begin
                bit_ind <= 0;
                we <= 0;
                state <= STOP;
@@ -106,5 +129,4 @@ always @(posedge scl) begin
 	   end
 	endcase
 end
-
 endmodule
--- i2c.srcs/sources_1/new/mstr.v
+++ i2c.srcs/sources_1/new/mstr.v
@@ -19,7 +19,7 @@ reg [4:0] mem_ind;
 reg [2:0] bit_ind;
 
 reg wd;
-reg slave_ans;
+// reg slave_ans;
 
 assign sda = we ? wd : 1'bz;
 
@@ -29,16 +29,23 @@ initial begin
     byte <= 8'b0;
     wd <= 1'b1;
     we <= 1'b1;
+    
+    for (integer i = 0; i < 6; i = i+1) begin
+        RAM[i] <= 8'b0;
+    end
 end 
 reg [3:0] state;
 	
-parameter   ADDR = 0,  
+parameter   ADDR = 0,  // for reading
 			RW = 1,   
 			RD_ACK = 2,   
 			RD_DATA = 3,  
 			SEND_ANS = 4, 
 			STOP = 5,
 			
+			IDLE = 8,
+			
+			RD_ACK_1 = 9,
 			WR_ACK = 7,
 			WR_DATA = 6;
 			 
@@ -61,14 +68,14 @@ always @(posedge scl) begin
                     wd <= address[bit_ind];
                     bit_ind <= bit_ind + 1'b1;
                     if (bit_ind == 3'd6)begin
-                        state <= RW;
                         bit_ind <= 0;
+                        state <= RW;
                     end
                 end
                 
                 RW: begin
                     we <= 1'b1;
-                    wd <= 1'b0;
+                    wd <= 1'b0; // read
                     we <= 1'b0;
                     // далее ведомый отвечает
                     state <= RD_ACK;
@@ -97,15 +104,75 @@ always @(posedge scl) begin
                     state <= STOP;
                 end
                 STOP: begin
+                    // nack signal
                     wd <= 0;
                     #5;
                     wd <= 1'b1;
                     #5;
                     wd <= 1'b0;
+                    state <= IDLE;
+                end
+                IDLE: begin
+                    wd <= 1;
                 end
             endcase
         end
-        1'b1: ;
+        1'b1: begin // writing
+            case(state)
+                ADDR: begin
+                    we <= 1'b1;
+                    // bit_ind <= 0;
+                    wd <= address[bit_ind];
+                    bit_ind <= bit_ind + 1'b1;
+                    if (bit_ind == 3'd6)begin
+                        bit_ind <= 0;
+                        state <= RW;
+                    end
+                end
+                RW: begin
+                    we <= 1'b1;
+                    wd <= 1'b1; // write
+                    we <= 1'b0;
+                    // далее ведомый отвечает
+                    state <= RD_ACK;
+                end
+                RD_ACK: begin
+                    if (sda == 1'b0) begin
+                        bit_ind <= 0;
+                        we <= 1'b1;
+                        state <= WR_DATA;
+                    end else state <= RD_ACK;
+                end
+                WR_DATA: begin
+                    wd <= RAM[mem_ind-1][bit_ind];
+                    bit_ind <= bit_ind + 1'b1;
+                    if (bit_ind == 3'd7) begin
+                        bit_ind <= 0;
+                        we <= 1'b0;
+                        state <= RD_ACK_1;
+                    end
+                end
+                RD_ACK_1: begin
+                    if (sda == 1'b0) begin
+                        bit_ind <= 0;
+                        we <= 1'b1;
+                        state <= STOP;
+                    end else state <= RD_ACK_1;
+                end
+                STOP: begin
+                    // nack signal
+                    wd <= 0;
+                    #5;
+                    wd <= 1'b1;
+                    #5;
+                    wd <= 1'b0;
+                    state <= IDLE;
+                end
+                IDLE: begin
+                    wd <= 1;
+                end
+            endcase
+        end
         default: ;
         endcase
     end
